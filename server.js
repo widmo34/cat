4df420d05560a8ea2530cb62c9906abed7f3e6e9
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-07-03 14:44:44 +0200

    13.8

---
+++ server.js
@@ -0,0 +1,25 @@
+// http 
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+
+
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+   
+    if (request.method === 'GET' && request.url === '/') {
+            
+        fs.readFile('/kodilla/cat/index.html', 'utf8', function(err, data){
+            response.write(data);
+            response.end();
+        })
+    } else {
+            response.statusCode = 404;
+            response.write('<h1>404: Zła ścieżka!</h1>');
+            response.end();
+    }
+});
+
+server.listen(8080);
